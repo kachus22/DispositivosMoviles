4a262d7c35f154cd7b552866db94551e2219db5e
HectorNLeon <hectornleon98@gmail.com>
2019-05-04 15:19:51 -0500

    Se agregaron archivos para el perfil

---
+++ app/src/main/java/mx/itesm/proyectofinal/PerfilActivity.kt
@@ -0,0 +1,66 @@
+/*
+    Copyright (C) 2018 - ITESM
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package mx.itesm.proyectofinal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_configuration.*
+
+// Configuration activity declaration and view inflation
+class PerfilActivity : AppCompatActivity() {
+
+    // Creates the activity and inflates the view
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configuration)
+        this.title = "Configuración"
+
+        var examplePrefs = getSharedPreferences("Shared", 0)
+        var editor = examplePrefs.edit()
+        switch1.isChecked = examplePrefs.getBoolean("Shared", false)
+
+        switch1.setOnCheckedChangeListener { buttonView, nurseChecked ->
+            editor.putBoolean("Shared", nurseChecked)
+            editor.commit()
+        }
+
+        if (PatientList.bluetoothHelper!!.isEnabled() && PatientList.bluetoothHelper!!.isDeviceConnected()) {
+            textView4.text = "El dispositivo de onda de pulso se encuentra conectado."
+        } else {
+            textView4.text = "El dispositivo de onda de pulso no se encuentra conectado."
+        }
+    }
+
+    // Handles clicking the back button
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                this.onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // Handles clicking the back button
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+}
