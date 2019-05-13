c388e6afb71d4a4eacf9b3e029f8be9203e6f7e7
Roberto Ramírez Monroy <38079022+rbtote@users.noreply.github.com>
2019-05-12 20:04:06 -0500

    Comentarios y modificación de estilos
    Agreados comentarios en funciones hechas
    Cambio de estilo en pantalla de buscar dispositivos
    Cambio de nombre de app

--- app/src/main/java/mx/itesm/proyectofinal/SharedPreference.kt
+++ app/src/main/java/mx/itesm/proyectofinal/SharedPreference.kt
@@ -9,7 +9,14 @@ class SharedPreference(val context: Context) {
     private val PREFS_NAME = "Preferences"
     val sharedPref: SharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
 
+    /**
+     * Series of functions that are used to save values in the app data, so they stay until changed
+     * or erased, even if the app is restarted.
+     */
 
+    /**
+     * Function to save objects of type profile, defined on signInActivity
+     */
     fun save(KEY_NAME: String, value: signInActivity.Companion.Profile) {
         val editor: SharedPreferences.Editor = sharedPref.edit()
 
@@ -20,7 +27,9 @@ class SharedPreference(val context: Context) {
         editor.commit()
     }
 
-
+    /**
+     * Function to save strings in the data
+     */
     fun save(KEY_NAME: String, value: String) {
         val editor: SharedPreferences.Editor = sharedPref.edit()
 
@@ -29,21 +38,26 @@ class SharedPreference(val context: Context) {
         editor.commit()
     }
 
+    /**
+     * Function to retrieve an object of type profilef rom the shared preferences
+     */
     fun getValueProfile(KEY_NAME: String): signInActivity.Companion.Profile? {
 
         return signInActivity.Companion.Profile(sharedPref.getString(KEY_NAME+"MAIL", null),sharedPref.getString(KEY_NAME+"NAME", null),sharedPref.getString(KEY_NAME+"IMG", null))
     }
 
+    /**
+     * Function to retrieve an string from the shared preferences
+     */
     fun getValueString(KEY_NAME: String): String? {
 
         return sharedPref.getString(KEY_NAME, null)
     }
 
-    fun getValueInt(KEY_NAME: String): Int {
 
-        return sharedPref.getInt(KEY_NAME, 0)
-    }
-
+    /**
+     * Function to clear all the saved data
+     */
     fun clearSharedPreference() {
 
         val editor: SharedPreferences.Editor = sharedPref.edit()
